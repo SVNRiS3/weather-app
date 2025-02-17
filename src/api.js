03a7a0ee48485f25cc05f8d53b8d9c26b3989f1e
SVNRiS3 <114239383+SVNRiS3@users.noreply.github.com>
2025-02-17 15:45:35 +0100

    Get the location

--- src/api.js
+++ src/api.js
@@ -4,8 +4,12 @@ const API_KEY = "77XKVNDA3MLAW8NWMJHKF3Q2Q";
 const API_KEY_URL = `?key=${API_KEY}`;
 
 const getLocationData = async (location) => {
-  const response = await fetch(SITE_URL + location + API_KEY_URL);
-  return response.json();
+  try {
+    const response = await fetch(SITE_URL + location + API_KEY_URL);
+    return response.json();
+  } catch (error) {
+    return console.log("Error: ", error);
+  }
 };
 
 export default getLocationData;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import "./style.css";
 import getLocationData from "./api";
 import Location from "./processing";
 
-const locationData = await getLocationData("Reda,pl");
-const currentLocation = new Location(locationData);
-console.log(currentLocation.getNow());
+const form = document.querySelector(".location-form");
+const userLocation = document.querySelector("#location");
+
+form.addEventListener("submit", (event) => {
+  getLocationData(userLocation.value).then((locationData) => {
+    const currentLocation = new Location(locationData);
+    console.log(currentLocation.getNow());
+  });
+  event.preventDefault();
+});
